2883230de0cd2b900051e6f64ab004fe652558d3
Manuel Pineda <pineda.manuel@gmail.com>
2022-04-21 18:25:55 -0500

    Questions --- Game questions included

---
+++ js/questions.js
@@ -0,0 +1,264 @@
+
+export const QUESTIONS = [
+    
+    [
+        {   //Level 1
+            "category" : "general culture",
+            "level" : 1,
+            "question" : "Cuál es el idioma oficial de Brasil?",
+            "a" : "Brasilero",
+            "b" : "Italiano",
+            "c" : "Portugues",
+            "d" : "Frances",
+            "answer" : "c"
+        },
+        {
+            "category" : "sports",
+            "level" : 1,
+            "question" : "Cada cuántos años se realizan los Juegos Olímpicos?",
+            "a" : "3 años",
+            "b" : "6 años",
+            "c" : "5 años",
+            "d" : "4 años",
+            "answer" : "d"
+        },
+        {
+            "category" : "geography",
+            "level" : 1,
+            "question" : "Cuál es la capital de El Salvador?",
+            "a" : "Managua",
+            "b" : "San Jose",
+            "c" : "San Salvador",
+            "d" : "Tegucigalpa",
+            "answer" : "c"
+        },
+        {
+            "category" : "sciences",
+            "level" : 1,
+            "question" : "Cuál es el cuarto planeta del sistema solar?",
+            "a" : "Tierra",
+            "b" : "Mercurio",
+            "c" : "Venus",
+            "d" : "Marte",
+            "answer" : "d"
+        },
+        {
+            "category" : "arts",
+            "level" : 1,
+            "question" : 'Quién escribió "Cien años de soledad"?',
+            "a" : "Rafael Pombo",
+            "b" : "José Eustasio Rivera",
+            "c" : "Jorge Isaacs",
+            "d" : "Gabriel García Márquez",
+            "answer" : "d"
+        }
+    ],
+    [
+        {   // Level 2
+            "category" : "general culture",
+            "level" : 2,
+            "question" : "El origen del café proviene de...",
+            "a" : "Etiopía",
+            "b" : "Egipto",
+            "c" : "Indonesia",
+            "d" : "Corea",
+            "answer" : "b"
+        },
+        {
+            "category" : "sports",
+            "level" : 2,
+            "question" : "Qué jugador de fútbol ha ganado más copas del mundo junto a su equipo?",
+            "a" : "Messi",
+            "b" : "Pelé",
+            "c" : "Maradona",
+            "d" : "Cristiano Ronaldo",
+            "answer" : "b"
+        },
+        {
+            "category" : "geography",
+            "level" : 2,
+            "question" : "Cuál es la montaña mas alta del mundo?",
+            "a" : "K2",
+            "b" : "Kanchenjunga",
+            "c" : "Manaslu",
+            "d" : "Everest",
+            "answer" : "d"
+        },
+        {
+            "category" : "sciences",
+            "level" : 2,
+            "question" : "Cuál es el elemento más abundante de la Tierra?",
+            "a" : "Oxigeno",
+            "b" : "Carbono",
+            "c" : "Hidrógeno",
+            "d" : "Potasio",
+            "answer" : "c"
+        },
+        {
+            "category" : "arts",
+            "level" : 2,
+            "question" : "Cuál fue la primera película que apareció de Disney?",
+            "a" : "Blancanieves",
+            "b" : "La bella y la bestia",
+            "c" : "Pinocho",
+            "d" : "Fantasía",
+            "answer" : "a"
+        }
+    ],
+    [
+        {   //Level 3
+            "category" : "general culture",
+            "level" : 3,
+            "question" : "En que año se fundó la franquicia de comida rápida McDonalds?",
+            "a" : "1945",
+            "b" : "1940",
+            "c" : "1954",
+            "d" : "1947",
+            "answer" : "a"
+        },
+        {
+            "category" : "sports",
+            "level" : 3,
+            "question" : "Qué animal es la mascota del Giro de Italia? ",
+            "a" : "Perro",
+            "b" : "Zorro",
+            "c" : "Alce",
+            "d" : "Lobo",
+            "answer" : "d"
+        },
+        {
+            "category" : "geography",
+            "level" : 3,
+            "question" : "Cuál es la capital de Marruecos?",
+            "a" : "Rabat",
+            "b" : "Kiev",
+            "c" : "Varsovia",
+            "d" : "Moscú",
+            "answer" : "a"
+        },
+        {
+            "category" : "sciences",
+            "level" : 3,
+            "question" : "Cómo se llama la unidad de intensidad de la corriente eléctrica?",
+            "a" : "Voltio",
+            "b" : "Amperio",
+            "c" : "Ohmio",
+            "d" : "Vatio",
+            "answer" : "b"
+        },
+        {
+            "category" : "arts",
+            "level" : 3,
+            "question" : "Dónde nació el escritor Julio Cortázar?",
+            "a" : "Uruguay",
+            "b" : "Argentina",
+            "c" : "Perú",
+            "d" : "Ecuador",
+            "answer" : "b"
+        }
+    ],
+    [
+        {   //Level 4
+            "category" : "general culture",
+            "level" : 4,
+            "question" : "Qué enfermedad tenía el legendario astrofísico Stephen Hawking?",
+            "a" : "Enfermedad de Addison",
+            "b" : "Progeria de Hutchinson-Gilford",
+            "c" : "Síndrome de Marfan",
+            "d" : "Esclerosis Lateral Amiotrófica",
+            "answer" : "d"
+        },
+        {
+            "category" : "sports",
+            "level" : 4,
+            "question" : "Cuál es el deporte en el que destacó Carl Lewis?",
+            "a" : "Atletismo",
+            "b" : "Gimnasia",
+            "c" : "Natación",
+            "d" : "Tenis",
+            "answer" : "a"
+        },
+        {
+            "category" : "geography",
+            "level" : 4,
+            "question" : "En qué pais se encuentra el rio Po?",
+            "a" : "Francia",
+            "b" : "España",
+            "c" : "Portugal",
+            "d" : "Italia",
+            "answer" : "d"
+        },
+        {
+            "category" : "sciences",
+            "level" : 4,
+            "question" : "Dónde están ubicados los huesos escafoides?",
+            "a" : "Cadera",
+            "b" : "Muñeca",
+            "c" : "Rodilla",
+            "d" : "pie",
+            "answer" : "b"
+        },
+        {
+            "category" : "arts",
+            "level" : 4,
+            "question" : "Quién es el director de la película Big Fish?",
+            "a" : "Peter Jackson",
+            "b" : "Steven Spielberg",
+            "c" : "Tim Burton",
+            "d" : "Christopher Nolan",
+            "answer" : "c"
+        }
+    ],
+    [
+        {   // Level 5
+            "category" : "general culture",
+            "level" : 5,
+            "question" : "Qué trilogía de las películas basadas en los cómics de Marvel recaudó más dinero en taquilla?",
+            "a" : "Iron Man",
+            "b" : "Wolverine",
+            "c" : "Capitán América",
+            "d" : "Thor",
+            "answer" : "c"
+        },
+        {
+            "category" : "sports",
+            "level" : 5,
+            "question" : "Cómo se llamaba el ciclista italiano apodado 'Il campionissimo'?",
+            "a" : "Fausto Coppi",
+            "b" : "Valerio Agnoli",
+            "c" : "Enrico Battaglin",
+            "d" : "Claudio Bortolotto",
+            "answer" : "a"
+        },
+        {
+            "category" : "geography",
+            "level" : 5,
+            "question" : "Cuál es la capital de Kazajistán?",
+            "a" : "Astaná",
+            "b" : "Taraz",
+            "c" : "Semey",
+            "d" : "Almatý",
+            "answer" : "a"
+        },
+        {
+            "category" : "sciences",
+            "level" : 5,
+            "question" : "Qué distancia hay de la Tierra al Sol?",
+            "a" : "Astaná",
+            "b" : "Taraz",
+            "c" : "Semey",
+            "d" : "Almatý",
+            "answer" : "a"
+        },
+        {
+            "category" : "arts",
+            "level" : 5,
+            "question" : "En qué año se celebró el festival de Woodstock?",
+            "a" : "1970",
+            "b" : "1969",
+            "c" : "1955",
+            "d" : "1976",
+            "answer" : "b"
+        }
+    ]
+];
