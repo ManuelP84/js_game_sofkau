182510d8d6b15e8f258fcfed76d4686a48a8bd05
Manuel Pineda <pineda.manuel@gmail.com>
2022-04-21 20:19:14 -0500

    Variables --- Variables creeation in logica.js

--- projectGame/js/logica.js
+++ projectGame/js/logica.js
@@ -1,1 +1,13 @@
-alert("hola");
+
+//variables correspondientes a los id y clases en HTML
+const categoria = document.querySelector('#categoria');
+const pregunta = document.querySelector('#pregunta');
+const opciones = Array.from(document.querySelectorAll('.textoRespuesta'));
+const progresoAviso = document.querySelector('#progreso');
+const puntos = document.querySelector('#puntos');
+const barraProgresoLlena = document.querySelector('#barraProgresoLlena');
+let preguntaActual = {}
+let respuestaObtenida= true;
+let puntaje = 0; //Puntaje inicial
+let contadorPreguntas = 0; //Contador de preguntas
+let preguntasDisponibles = []
