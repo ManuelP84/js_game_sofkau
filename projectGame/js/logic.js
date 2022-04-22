e9590b6188296fd52abe0d1fc285585a638aa1d2
Granadalook <tiven17.jsga@gmail.com>
2022-04-22 17:58:34 -0500

    projectRefactor:  se crea modal para  respuesta incorrecta   y  agregan estilos al mismo

--- projectGame/js/logic.js
+++ projectGame/js/logic.js
@@ -68,15 +68,17 @@ opciones.forEach((opcion) => {
     if (classToApply === "correcto") {
       incrementarPuntaje(PUNTOS_CORRECTA);
     } else {
-      window.alert("¡Has perdido!");
-      terminoJuego();
+      activarBoton();
+      setTimeout(() => {
+        terminoJuego();
+      }, 2000);
     }
     opcionSeleccionada.parentElement.classList.add(classToApply);
 
     setTimeout(() => {
       opcionSeleccionada.parentElement.classList.remove(classToApply);
       obtenerNuevaPregunta();
-    }, 1000);
+    }, 3000);
   });
 });
 
@@ -89,4 +91,41 @@ let terminoJuego = () => {
   localStorage.setItem("puntajeMasReciente", puntaje);
   return window.location.assign("../template/endGame.html");
 };
+
+function activarBoton() {
+  let llamado = document.getElementById("btnModal");
+  llamado.click();
+}
+
+if (document.getElementById("btnModal")) {
+  var modal = document.getElementById("myModal");
+  var boton1 = document.getElementById("btnModal");
+  var span = document.getElementsByClassName("close")[0];
+  var body = document.getElementsByTagName("body")[0];
+
+  boton1.onclick = function () {
+    modal.style.display = "block";
+    body.style.position = "static";
+    body.style.height = "100%";
+    body.style.overflow = "hidden";
+  };
+  span.onclick = function () {
+    modal.style.display = "none";
+
+    body.style.position = "inherit";
+    body.style.height = "auto";
+    body.style.overflow = "visible";
+  };
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+
+      body.style.position = "inherit";
+      body.style.height = "auto";
+      body.style.overflow = "visible";
+    }
+  };
+}
+
 empezarJuego();
